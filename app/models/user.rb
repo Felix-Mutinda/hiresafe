848d23619df600518999ccb3d60635ccdb5cf100
Felix-Mutinda <1mutindafelix@gmail.com>
2019-01-13 22:24:59 +0300

    update file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-	validates :nat_id, presence: true,
-				length: { minimum: 8, maximum: 8 }
-	validates :name, presence: true
+	
 end
