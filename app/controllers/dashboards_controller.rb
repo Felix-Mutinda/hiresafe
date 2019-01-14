276abb0e8ebfbb61a0719b8e8c0fe65702a01f39
Felix-Mutinda <1mutindafelix@gmail.com>
2019-01-14 21:25:19 +0300

    separate owner/client dashboards

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,12 @@ class DashboardsController < ApplicationController
 	
 	def index
 		@user = current_user
-		byebug
+		
+		if current_user.user_type == "1" # client
+			render 'dashboards/client_index'
+			
+		else #owner
+			render 'dashboards/owner_index'
+		end
 	end
 end
