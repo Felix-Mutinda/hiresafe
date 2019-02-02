a2775f2a86ed001ce6c36de8ab09b6d36d62e5b2
Felix-Mutinda <1mutindafelix@gmail.com>
2019-02-02 18:25:39 +0300

    access dashboard/ search without login

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,37 @@
 class DashboardsController < ApplicationController
-	
+	skip_before_action :authenticate_user!, only: [:index, ]
 	
 	def index
+		@user = current_user
+		@car = Car.first
+		
+		@available_cars = []
+		hired = false
+		
+		Car.all.each do |car|
+			
+			HiredCar.all.each do |hired_car|
+				if car.id == hired_car.car_id
+					hired = true
+					break
+				end
+			end
+			
+			
+			if !hired
+				@available_cars.push(car)
+			end
+			hired = false
+		end
+	
+		if @user 
+			@list_car_path = new_user_car_path(@user)
+		else
+			@list_car_path = new_user_session_path
+		end
+	end
+	
+	def index_old
 		@user = current_user
 		@car = Car.first
 		
