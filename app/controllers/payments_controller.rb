bf98bee050ac74ff94578095ebfce52be0de2ad0
= <=>
2019-03-06 08:09:37 +0000

    payments access token method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,31 @@
 class PaymentsController < ApplicationController
+    require 'http'
+    require 'https'
+    require 'uri'
+    require 'json'
+    
+    # important constants
+    CONSUMER_KEY = 'Qc2tfjP3fDZE7XXveM2SkxIbyy8XoH2f'
+    CONSUMER_SECRET = 'Sg8WZUtVgbXAPnUn'
+    
+    
+private
+    def access_token
+        uri = URI('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials')
+        response = nil
+        
+        Net::HTTP.start(uri.host, uri.port,
+          :use_ssl => uri.scheme == 'https',
+          :verify_mode => OpenSSL::SSL::VERIFY_NONE)    do |http|
+        
+          request = Net::HTTP::Get.new uri.request_uri
+          request.basic_auth CONSUMER_KEY, CONSUMER_SECRET
+        
+          response = http.request request # Net::HTTPResponse object
+        end
+        
+        response_json = JSON.parse(response.body)
+        response_json['access_token']
+    end
+            
 end
