d037a172a0daac122637b3f6c8a37ad625f2db88
Felix-Mutinda <1mutindafelix@gmail.com>
2019-03-19 09:44:57 +0000

    minor update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :mname,:lname, :nat_id, :dl_no, :mobile, :avatar])
-            devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :mname,:lname, :user_type, :avatar])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :mname,:lname, :user_type, :mobile, :avatar])
         end
 end
